cb13c783054d62829bb29b87309390512b437254
Vincent Fahrenholz <vincent.fahrenholz@idealo.de>
2021-12-03 10:09:07 +0100

    fix startday-script

--- startday.sh
+++ startday.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-echo $1
 current=$1
 days=(zero test two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twentyone twentytwo twentythree twentyfour twentyfive)
 
 mkdir "day${days[current]}"
-cp main_template.txt "day${days[current]}/main.go"                                                               ─╯
+cp main_template.txt "day${days[current]}/main.go"
 
 
